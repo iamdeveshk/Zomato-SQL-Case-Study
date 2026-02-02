38ce66d161bccfb12f120abb3ff4c78f3cba3842
DEVESH KUMAR <230137005@hbtu.ac.in>
2026-02-02 19:19:08 +0530

    Update zomato case study.sql

--- zomato case study.sql
+++ zomato case study.sql
@@ -1,48 +1,48 @@
--- 1.Select a particular Database 
+-- Select a particular Database 
 USE zomato;
 
 SELECT * FROM users;
--- 2.Count number of rows
+-- Count number of rows
 SELECT COUNT(*) FROM order_details;
 
--- 3. Return n random records
+-- Return n random records
 -- replicated sample funtion from pandas
 SELECT * FROM users  ORDER BY rand() LIMIT 5; #random funtion --> random n rows
 
--- 4. Find NULL Values
+-- Find NULL Values
 SELECT * FROM orders WHERE restaurant_rating IS NULL;
 
 -- To replace NULL values with 0
 UPDATE orders 
 SET restaurant_rating = 0 WHERE restaurant_rating IS NULL;
 
--- 5.Find the number of orders placed by each customers
+-- Find the number of orders placed by each customers
 SELECT t1.name, COUNT(*) AS '#orders' FROM users t1
 JOIN orders t2
 ON t1.user_id = t2.user_id
 GROUP BY t1.user_id,t1.name;
 
---  6.Find restaurant with most number of menu items 
+--  Find restaurant with most number of menu items 
 SELECT r_name, COUNT(*) AS 'menu_items' FROM restaurants t1
 JOIN menu t2
 ON t1.r_id = t2.r_id
 GROUP BY t2.r_id, r_name;
 
--- 7.Find number of votes and avg rating for all the restaurants
+-- Find number of votes and avg rating for all the restaurants
 SELECT t2.r_name,COUNT(*) AS 'num_votes', AVG(restaurant_rating) AS 'avg_rating' FROM orders t1
 JOIN restaurants t2
 ON t1.r_id = t2.r_id
 WHERE restaurant_rating IS NOT NULL
 GROUP BY t1.r_id,r_name;
 
--- 8.Find the food that is being sold at most number of restaurants
+-- Find the food that is being sold at most number of restaurants
 SELECT t2.f_name,COUNT(*) FROM menu t1
 JOIN food t2
 ON t1.f_id = t2.f_id
 GROUP BY t1.f_id, t2.f_name
 ORDER BY COUNT(*) DESC LIMIT 1;
 
--- 9.Find restaurant with max revenue in a given month
+-- Find restaurant with max revenue in a given month
 -- > May
 SELECT DATE(date) FROM orders; -- changing text to DATE format
 SELECT MONTH(DATE(date)),date FROM orders; -- extracting month correspondingly
@@ -64,20 +64,20 @@ WHERE r_name = 'kfc'
 GROUP BY MONTHNAME(DATE(date))
 ORDER BY MONTH(DATE(date));
 
--- 10. Find restaurant with sales> x
+-- Find restaurant with sales> x
 SELECT r_name, SUM(amount) AS 'revenue'  FROM orders t1
 JOIN restaurants t2
 ON t1.r_id = t2.r_id
 GROUP BY t2.r_id HAVING revenue > 1500;
 
--- 11. Find customers who have never ordered (using SQL SET operators) A-B
+-- Find customers who have never ordered (using SQL SET operators) A-B
 SELECT user_id,name FROM users
 EXCEPT
 (SELECT t1.user_id,t2.name FROM orders t1
 JOIN users t2
 ON t1.user_id= t2.user_id);
 
--- 12.Show order details of a particular customer in a given date range 
+-- Show order details of a particular customer in a given date range 
 -- range: 15May- 15 June
 SELECT t1.order_id,f_name FROM orders t1
 JOIN order_details t2
@@ -87,21 +87,21 @@ ON t2.f_id = t3.f_id
 WHERE user_id = 4 AND date BETWEEN '2022-05-15' AND '2022-06-15'; -- by default MYSQL filters date in a string format
 
 
--- 14.Find most costly restaurants (Avg price / dish)
+-- Find most costly restaurants (Avg price / dish)
 SELECT r_name, SUM(price)/COUNT(*) FROM menu t1
 JOIN restaurants t2
 ON t1.r_id = t2.r_id
 GROUP BY t1.r_id
 ORDER BY SUM(price)/COUNT(*) DESC;
 
--- 15.FInd delivery partner compensation using the formula (#deliveries*100 + 10000*avg_rating)
+-- FInd delivery partner compensation using the formula (#deliveries*100 + 10000*avg_rating)
 SELECT partner_name, (COUNT(*)*100 + AVG(delivery_rating) * 1000) AS 'salaries' FROM orders t1
 JOIN  delivery_partner t2
 ON t1.partner_id = t2.partner_id
 GROUP BY t1.partner_id
 ORDER BY salaries DESC;
 
--- 17.Find all the veg restaurants
+-- Find all the veg restaurants
 SELECT t3.r_id,t3.r_name FROM menu t1
 JOIN food t2
 ON t1.f_id = t2.f_id
@@ -110,7 +110,7 @@ on t1.r_id = t3.r_id
 GROUP BY t1.r_id
 HAVING MIN(type) = 'Veg' AND MAX(type) = 'Veg';
 
--- 18. Find min and max order value for all the customers
+-- Find min and max order value for all the customers
 SELECT name,MIN(amount),MAX(amount), AVG(amount) FROM orders t1
 JOIN users t2 
 ON t1.user_id = t2.user_id
